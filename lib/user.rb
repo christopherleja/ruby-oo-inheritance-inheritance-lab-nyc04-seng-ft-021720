d98ef5c11420f28d62d630d14cf0510129a627ef
Christopher Leja <christopher.leja@gmail.com>
2020-03-01 15:16:37 +0000

    Automatically backed up by Learn

--- lib/user.rb
+++ lib/user.rb
@@ -1,10 +1,10 @@
 class User
   
-  attr_accessor :first_name, :last_name
+  attr_accessor :first, :last
   
   @@all = []
   
-  def initialize(first_name, last_name)
+  def initialize(first, last)
     @first_name = first_name
     @last_name = last_name
   end
