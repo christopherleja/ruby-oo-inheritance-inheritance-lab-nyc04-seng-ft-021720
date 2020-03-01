ee123535f311e3ed6a8c2d8afda953d0adfdf71f
Christopher Leja <christopher.leja@gmail.com>
2020-03-01 15:19:24 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,4 +1,15 @@
 require_relative './user.rb'
 class Student < User
-
+  
+  def initialize
+    knowledge_array = []
+  end
+  
+  def learn(knowledge)
+    knowledge_array.push(knowledge)
+  end
+  
+  def knowledge
+    knowledge_array
+  end
 end
